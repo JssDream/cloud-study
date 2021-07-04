fb1dcc29ded449d8d7463cad676cfb743bf030bd
jiaoshao <190655106@qq.com>
2021-07-04 18:19:47 +0800

    Eureka

---
+++ cloud-eureka-server/src/main/java/com/shao/cloud/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.shao.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务
+ *
+ * Created by Js on 2021/6/30 .
+ **/
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
